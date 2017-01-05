a7fa1be2ad2013f611fffb0659f572a04e227fa0
Toseefhusen Khilji <toseefhusenkhilji@mac-mini3.local>
2017-01-05 18:10:41 +0530

    Initial Commit

---
+++ TestWebserviceSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestWebserviceSwift
+//
+//  Created by Toseefhusen Khilji on 05/01/17.
+//  Copyright © 2017 Toseef Khilji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
